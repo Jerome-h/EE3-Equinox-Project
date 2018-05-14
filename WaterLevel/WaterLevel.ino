606715195301495521db8d5890f9728853c649ff
Jerome-h <Jerome-h@users.noreply.github.com>
2018-05-14 18:19:42 +0100

    data stored in tank structure

--- WaterLevel/WaterLevel.ino
+++ WaterLevel/WaterLevel.ino
@@ -1,38 +1,66 @@
-// Volume of water tank 2. Enter dimensions in metres.
-const int tankWidth = 1;
-const int tankLength = 1; 
-const int tankHeight = 1;
+// Ultrasonic Pins
+const int tank2Echo = 6;
+const int tank2Trig = 7;
+
+struct tank {
+  const float tankHeight;
+  const float tankDiameter;
+  float waterDistance;
+  float waterHeight;
+  float waterVolume;
+};
+
+//Initialises tanks being monitored. Input: {Height, Diameter}
+struct tank tank2 = {0.095, 0.075} ;
+
+void tankCapacity(struct tank &Tank, const int trigPin, const int echoPin);
 
 void setup() {
-  Serial.begin(9600); // Serial begin texting 
+  Serial.begin(9600); // Serial begin texting
+  pinMode (tank2Echo,  INPUT);
+  pinMode (tank2Trig, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  tankCapacity(tank2, tank2Trig, tank2Echo);
 
+  Serial.print("Water distance: ");
+  Serial.print(tank2.waterDistance);
+  Serial.println("m");
+  Serial.print("Water height: ");
+  Serial.print(tank2.waterHeight);
+  Serial.println("m");
+  Serial.print("Water Volume: ");
+  Serial.print(tank2.waterVolume);
+  Serial.println("L");
+  Serial.println();
+
+  delay(500);
 }
 
-long WaterLevel(const int trigPin, const int echoPin) {
-  long duration , distance, waterHeight, waterVolume ;
-  
+void tankCapacity(struct tank &Tank, const int trigPin, const int echoPin) {
+  float duration , waterDistance, waterHeight, waterVolume ;
+
   // Clears the trigPin for two microseconds
-  digitalWrite(trigPin, OUTPUT); 
+  digitalWrite(trigPin, OUTPUT);
   delayMicroseconds(2);
-  
   // sets trigPin for 10 microseconds
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
-  
   // Calculates distance using: D = S*T
   duration = pulseIn(echoPin, HIGH); // Waits for echo pin to go high which starts the timing
-                                     // When echo pin goes low the timing stops
-                                     // Returns the duration of the pulse in microseconds
-  distance = duration*0.34/2;       // Calculates the distance in metres
-      
-  waterHeight = tankHeight - distance;
-  waterVolume = waterHeight * tankWidth * tankLength;
-  return waterVolume;
+  // When echo pin goes low the timing stops
+  // Returns the duration of the pulse in microseconds
+  waterDistance = duration * 0.000343 / 2 ;   // Calculates the distance in metres
+  Tank.waterDistance = waterDistance;
 
+  waterHeight = Tank.tankHeight - waterDistance;
+  Tank.waterHeight = waterHeight;
+
+  waterVolume = waterHeight * 3.14 * sq(Tank.tankDiameter / 2) * 1000; // V = height * pi * r^2 * litre conversion
+  Tank.waterVolume = waterVolume;
+
+  return Tank;
 }
 
