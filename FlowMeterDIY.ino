80e21b35a3cb12fc9e3d0df2ac9e92b02482a2ca
skeyang <sky15@ic.ac.uk>
2018-05-16 13:03:18 +0100

    Update FlowMeterDIY.ino

--- FlowMeterDIY.ino
+++ FlowMeterDIY.ino
@@ -1,12 +1,3 @@
-/*
-Liquid flow rate sensor -DIYhacking.com Arvind Sanjeev
-
-Measure the liquid/water flow rate using this code. 
-Connect Vcc and Gnd of sensor to arduino, and the 
-signal line to arduino digital pin 2.
- 
- */
-
 byte statusLed    = 13;
 byte sensorInterrupt = 0;  // 0 = digital pin 2
 byte sensorPin       = 2;
@@ -15,12 +6,19 @@ byte sensorPin       = 2;
 // litre/minute of flow.
 float calibrationFactor = 4.5;
 
+struct flow {
+  float flowRate;
+  unsigned int flowMilliLitres;
+  unsigned long totalMilliLitres;
+  unsigned int frac;
+}waterFlow;
+
 volatile byte pulseCount;  
-float flowRate;
-unsigned int flowMilliLitres;
-unsigned long totalMilliLitres;
-unsigned int frac;
 unsigned long oldTime;
+
+void pulseCounter();
+void flowInfo(volatile byte &pulseCount, struct flow &info, unsigned long &oldTime);
+void printFlow(struct flow &info);
 
 void setup(){
   // Initialize a serial connection for reporting values to the host
@@ -33,11 +31,11 @@ void setup(){
   pinMode(sensorPin, INPUT);
   digitalWrite(sensorPin, HIGH);
 
-  pulseCount        = 0;
-  flowRate          = 0.0;
-  flowMilliLitres   = 0;
-  totalMilliLitres  = 0;
-  oldTime           = 0;
+  pulseCount                  = 0;
+  waterFlow.flowRate          = 0.0;
+  waterFlow.flowMilliLitres   = 0;
+  waterFlow.totalMilliLitres  = 0;
+  oldTime                     = 0;
 
   // The Hall-effect sensor is connected to pin 2 which uses interrupt 0.
   // Configured to trigger on a FALLING state change (transition from HIGH
@@ -48,13 +46,10 @@ void setup(){
 /**
  * Main program loop
  */
-
-void flowInfo(volatile byte &pulseCount, float &flowRate, unsigned int &flowMilliLitres, unsigned long &totalMilliLitres, unsigned int &frac, unsigned long &oldTime);
-void printFlow(float &flowRate, unsigned int &flowMilliLitres, unsigned long &totalMilliLitres, unsigned int &frac);
 void loop(){
-  if((millis() - oldTime) > 1000)    // Only process counters once per second{  
-    flowInfo(pulseCount, flowRate, flowMilliLitres, totalMilliLitres, frac, oldTime);  
-    printFlow(flowRate, flowMilliLitres, totalMilliLitres, frac); 
+  if((millis() - oldTime) > 1000){    // Only process counters once per second
+    flowInfo(pulseCount, waterFlow, frac, oldTime);  
+    printFlow(waterFlow, frac); 
   }
 }
 
@@ -66,7 +61,8 @@ void pulseCounter(){
   pulseCount++;
 }
 
-void flowInfo(volatile byte &pulseCount, float &flowRate, unsigned int &flowMilliLitres, unsigned long &totalMilliLitres, unsigned int &frac, unsigned long &oldTime){
+// calculates flow rate, current flow rate and total flow output 
+void flowInfo(volatile byte &pulseCount, struct flow &info, unsigned long &oldTime){
   // Disable the interrupt while calculating flow rate and sending the value to
   // the host
   detachInterrupt(sensorInterrupt);
@@ -76,7 +72,7 @@ void flowInfo(volatile byte &pulseCount, float &flowRate, unsigned int &flowMill
   // that to scale the output. We also apply the calibrationFactor to scale the output
   // based on the number of pulses per second per units of measure (litres/minute in
   // this case) coming from the sensor.
-  flowRate = ((1000.0 / (millis() - oldTime)) * pulseCount) / calibrationFactor;
+  info.flowRate = ((1000.0 / (millis() - oldTime)) * pulseCount) / calibrationFactor;
   
   // Note the time this processing pass was executed. Note that because we've
   // disabled interrupts the millis() function won't actually be incrementing right
@@ -87,13 +83,13 @@ void flowInfo(volatile byte &pulseCount, float &flowRate, unsigned int &flowMill
   // Divide the flow rate in litres/minute by 60 to determine how many litres have
   // passed through the sensor in this 1 second interval, then multiply by 1000 to
   // convert to millilitres.
-  flowMilliLitres = (flowRate / 60) * 1000;
+  info.flowMilliLitres = (info.flowRate / 60) * 1000;
     
   // Add the millilitres passed in this second to the cumulative total
-  totalMilliLitres += flowMilliLitres;
+  info.totalMilliLitres += info.flowMilliLitres;
   
   // Determine the fractional part. The 10 multiplier gives us 1 decimal place.     
-  frac = (flowRate - int(flowRate)) * 10;
+  info.frac = (info.flowRate - int(info.flowRate)) * 10;
        
   // Reset the pulse counter so we can start incrementing again
   pulseCount = 0;
@@ -101,19 +97,23 @@ void flowInfo(volatile byte &pulseCount, float &flowRate, unsigned int &flowMill
   // Enable the interrupt again now that we've finished sending output
   attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
 }
-void printFlow(float &flowRate, unsigned int &flowMilliLitres, unsigned long &totalMilliLitres, unsigned int &frac){
+
+// print flow rate, current flow rate and total flow output 
+void printFlow(struct flow &info){
   // Print the flow rate for this second in litres / minute
   Serial.print("Flow rate: ");
-  Serial.print(int(flowRate));  // Print the integer part of the variable
+  Serial.print(int(info.flowRate));  // Print the integer part of the variable
   Serial.print(".");             // Print the decimal point
-  Serial.print(frac, DEC) ;      // Print the fractional part of the variable
+  Serial.print(info.frac, DEC) ;      // Print the fractional part of the variable
   Serial.print("L/min");
+  
   // Print the number of litres flowed in this second
   Serial.print("  Current Liquid Flowing: ");             // Output separator
-  Serial.print(flowMilliLitres);
+  Serial.print(info.flowMilliLitres);
   Serial.print("mL/Sec");
-   // Print the cumulative total of litres flowed since starting
+  
+  // Print the cumulative total of litres flowed since starting
   Serial.print("  Output Liquid Quantity: ");             // Output separator
-  Serial.print(totalMilliLitres);
+  Serial.print(info.totalMilliLitres);
   Serial.println("mL");   
 }
