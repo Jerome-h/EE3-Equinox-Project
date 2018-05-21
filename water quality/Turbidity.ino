871fa7024ccb619ad3567c08898706161faf9236
skeyang <sky15@ic.ac.uk>
2018-05-21 15:26:11 +0100

    Create Turbidity.ino

---
+++ water quality/Turbidity.ino
@@ -0,0 +1,29 @@
+/*
+ * For now I'm still trying to find out the relationship between the turbidity and the voltage of the
+ * analog pin, the relationship given in the website is not accurate. Right now I can only give 
+ * ranges of voltage as estimates.
+ * 4V - 5V ~= 0 NTU --> Clean water
+ * 3.5V - 4V ~= 1000 NTU
+ * 2V - 3V ~= 3000 NTU
+*/
+
+int pin = A0; // analog pin 
+int turbidityValue;
+float voltage;
+void setup() {
+  Serial.begin(9600); //Baud rate: 9600
+}
+
+void turbidity(int pin, float &voltage);
+
+void loop() {
+  turbidity(pin, NTU, voltage);
+  Serial.print("Voltage = ");
+  Serial.println(voltage); // print out the value you read:
+  delay(300);
+}
+
+void turbidity(int pin, float &voltage){
+  int sensorValue = analogRead(pin);// read the input on analog pin 0:
+  voltage = sensorValue * (5 / 1024.0); // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
+}
