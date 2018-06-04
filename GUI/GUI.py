5e0440cb9577100410c35e6c6f41c426ec96ff9b
Jerome-h <Jerome-h@users.noreply.github.com>
2018-06-04 15:29:07 +0100

    FTP read file

---
+++ GUI/GUI.py
@@ -0,0 +1,43 @@
+from matplotlib import pyplot as plt
+import ftplib
+
+
+def getfile(ftp, filename):
+    try:
+        ftp.retrbinary("RETR " + filename, open(filename, 'wb').write)
+
+    except:
+        print
+        "Error"
+
+
+# Open ftp connection
+ftp = ftplib.FTP('ftp.drivehq.com', 'equinox_eee', 'equinox1234')
+
+# List the files in the current directory
+print("File List:")
+files = ftp.dir()
+print(files)
+
+ftp.cwd('/')  # change directory to /pub/
+getfile(ftp, 'data.txt')
+ftp.close()
+# Print the readme file contents
+print("\nData File Output:")
+gFile = open("data.txt", "r")
+buff = gFile.read()
+print(buff)
+gFile.close()
+
+
+# -*- coding: utf-8 -*-
+
+def graph(temps, refresh):
+    plt.plot(temps, 'r', label='^C', marker='o')
+    plt.legend()
+
+    axes = plt.gca()
+
+    plt.draw()
+    plt.pause(refresh)
+    plt.clf()
