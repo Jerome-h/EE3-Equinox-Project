077fa15322c1b64a1b8bca5c69ceb431dea7c1ba
Jerome-h <Jerome-h@users.noreply.github.com>
2018-06-04 13:43:53 +0100

    AT communication, no FTP yet

--- WiFi_Send/WiFi_Send.ino
+++ WiFi_Send/WiFi_Send.ino
@@ -32,7 +32,7 @@ void setup() {
   connectWifi();
 }
 
-bool sendATcommand(String& ATcommand, String& expected_answer, unsigned int& timeout) {
+bool sendATcommand(const String& ATcommand, const char& expected_answer, unsigned int& timeout) {
   esp.println(ATcommand);
   delay(timeout);
   if (esp.find(expected_answer) ) return true;
@@ -80,96 +80,7 @@ bool motionDetect() {
 
 void loop () {
   if (motionDetect ()) {
-    httppost();
   }
   delay(1000);
 }
 
-void configure_FTP() {
-
-  sendATcommand("AT+SAPBR=3,1,"Contype","GPRS"", "OK", 2000);
-  sendATcommand("AT+SAPBR=3,1,"APN","APN"", "OK", 2000);
-  sendATcommand("AT+SAPBR=3,1,"USER","user_name"", "OK", 2000);
-  sendATcommand("AT+SAPBR=3,1,"PWD","password"", "OK", 2000);
-
-  while (sendATcommand("AT+SAPBR=1,1", "OK", 20000) != 1);
-  sendATcommand("AT+FTPCID=1", "OK", 2000);
-  sendATcommand("AT+FTPSERV="ftp.yourserver.com"", "OK", 2000);
-  sendATcommand("AT+FTPPORT=21", "OK", 2000);
-  sendATcommand("AT+FTPUN="user_name"", "OK", 2000);
-  sendATcommand("AT+FTPPW="password"", "OK", 2000);
-
-}
-
-
-void uploadFTP() {
-
-  sendATcommand("AT+FTPPUTNAME="file_name"", "OK", 2000); // AT+FTPPUTNAME command sets up the FTP upload file name
-  sendATcommand("AT+FTPPUTPATH=" / path"", "OK", 2000);   // T+FTPPUTPATH sets up the path name of the upload file
-
-  // AT+FTPPUT sets up the FTP upload
-  // mode '1' is to open FTP put session
-  if (sendATcommand("AT+FTPPUT=1", "+FTPPUT:1,1,", 30000) == 1) {
-    data_size = 0;
-    while (Serial.available() == 0);
-    aux = Serial.read();
-
-    // loops while aux is not carriage Return
-    do {
-      data_size *= 10;
-      data_size += (aux - 0x30);
-      while (Serial.available() == 0);
-      aux = Serial.read();
-    } while (aux != 0x0D);
-
-    if (data_size >= 100) {
-
-      // mode '2' is for write FTP upload data
-      if (sendATcommand("AT+FTPPUT=2,100", "+FTPPUT:2,100", 30000) == 1) {
-        Serial.println(sendATcommand(testString, "+FTPPUT:1,1", 30000), DEC);
-        Serial.println(sendATcommand("AT+FTPPUT=2,0", "+FTPPUT:1,0", 30000), DEC);
-        Serial.println("Upload done!!");
-      }
-      else {
-        sendATcommand("AT+FTPPUT=2,0", "OK", 30000);
-      }
-    }
-    else {
-      sendATcommand("AT+FTPPUT=2,0", "OK", 30000);
-    }
-  }
-  else {
-    Serial.println("Error openning the FTP session");
-  }
-}
-
-void httppost () {
-  String data = "Motion Detected!";
-  esp.println("AT+CIPSTART=\"TCP\",\"" + server + "\",80");//start a TCP connection.
-  if ( esp.find("OK")) {
-    Serial.println("TCP connection ready");
-  } delay(1000);
-  String postRequest =
-    "POST " + uri + " HTTP/1.0\r\n" +
-    "Host: " + server + "\r\n" +
-    "Accept: *" + "/" + "*\r\n" +
-    "Content-Length: " + data.length() + "\r\n" +
-    "Content-Type: application/x-www-form-urlencoded\r\n" +
-    "\r\n" + data;
-  String sendCmd = "AT+CIPSEND=";//determine the number of characters to be sent.
-  esp.print(sendCmd);
-  esp.println(postRequest.length() );
-  delay(500);
-  if (esp.find(">")) {
-    Serial.println("Sending.."); esp.print(postRequest);
-    if ( esp.find("SEND OK")) {
-      Serial.println("Packet sent");
-      while (esp.available()) {
-        String tmpResp = esp.readString();
-        Serial.println(tmpResp);
-      }
-      // close the connection
-      esp.println("AT+CIPCLOSE");
-    }
-  }
-}
