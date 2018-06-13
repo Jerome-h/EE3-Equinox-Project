51f24a96e560abd0008f0157ea03c6f8b6a1ce2c
skeyang <sky15@ic.ac.uk>
2018-06-13 18:04:48 +0100

    Update LiveGraph.py

--- GUI/LiveGraph.py
+++ GUI/LiveGraph.py
@@ -75,20 +75,20 @@ def getRange(startDate, endDate, dictionary, dictionaryRange):
 def graphLine(dictionary, xlabel, ylabel, title, refresh, ymax, row, column, index, colour):
     lists = sorted(dictionary.items())
     x, y = zip(*lists)
-    x = x[-21:] # Extract last 21 values to plot
-    y = y[-21:]
+    x = x[-41:] # Extract last 21 values to plot
+    y = y[-41:]
     xn = range(len(x))  # map your string labels to integers
     plt.subplot(row, column, index)
     plt.cla()
     plt.ylim(0,ymax)
-    plt.plot(xn, y, colour, marker='o')
+    plt.plot(xn, y, colour)
     plt.subplots_adjust(bottom=0.15)
     plt.xlabel('{}'.format(xlabel)) 
     plt.ylabel('{}'.format(ylabel))
     plt.title('{}'.format(title))
     xlabels = list(x)
     for index, item in enumerate(xlabels):
-        if (index % 2):
+        if (index % 4):
             xlabels[index] = " "
     plt.xticks(xn, xlabels)   # set it to the string values
     plt.xticks(rotation=45)
@@ -117,6 +117,9 @@ while True:
         dictLevel = dict()
         dictTurb = dict()
         dictFlow = dict()
+        dictPH = dict()
+        dictEC = dict()
+        dictTemp = dict()
         
         getfile(ftpserver, 'waterOld.csv')
 
@@ -142,25 +145,28 @@ while True:
             ftpserver.delete('waterNew.csv')
             
                 
-        plt.figure('Water Measurements')                                   
+        plt.figure('Water Measurements')
+        
         readfile('waterOld.csv', 'Level', dictLevel)
         graphLine(dictLevel, 'Date and Time', 'Level', 'Water Level', 0.1, 0.4, 2, 3, 1, 'b')
 
-        readfile('waterOld.csv', 'Turb', dictTurb)
-        graphLine(dictTurb, 'Date and Time', 'Turbidity', 'Turbidity', 0.1, 3100, 2, 3, 2, 'g')
-
         readfile('waterOld.csv', 'Flow 2', dictFlow)
-        graphLine(dictFlow, 'Date and Time', 'Flow Rate', 'Flow Rate', 0.1, 50, 2, 3, 3, 'c')
+        graphLine(dictFlow, 'Date and Time', 'Flow Rate', 'Flow Rate', 0.1, 50, 2, 3, 2, 'c')
 
-        readfile('waterOld.csv', 'PH', dictFlow)
-        graphLine(dictFlow, 'Date and Time', 'pH', 'pH', 0.1, 14, 2, 3, 4, 'm')
+        readfile('waterOld.csv', 'Turb', dictTurb)
+        graphLine(dictTurb, 'Date and Time', 'Turbidity', 'Turbidity', 0.1, 3100, 2, 3, 3, 'g')
 
-        readfile('waterOld.csv', 'Temp', dictFlow)
-        graphLine(dictFlow, 'Date and Time', 'Temperature', 'Temperature', 0.1, 80, 2, 3, 5, 'r')
+        readfile('waterOld.csv', 'PH', dictPH)
+        graphLine(dictPH, 'Date and Time', 'pH', 'pH', 0.1, 14, 2, 3, 4, 'm')
 
-        readfile('waterOld.csv', 'EC', dictFlow)
-        graphLine(dictFlow, 'Date and Time', 'EC', 'Electric Conductivity', 0.1, 5000, 2, 3, 6, 'k')
+        readfile('waterOld.csv', 'Temp', dictTemp)
+        graphLine(dictTemp, 'Date and Time', 'Temperature', 'Temperature', 0.1, 80, 2, 3, 5, 'r')
+
+        readfile('waterOld.csv', 'EC', dictEC)
+        graphLine(dictEC, 'Date and Time', 'EC', 'Electric Conductivity', 0.1, 5000, 2, 3, 6, 'k')
+
         plt.tight_layout()
+        
         #plt.cla()
         uploadfile(ftpserver, 'waterOld.csv')
 
