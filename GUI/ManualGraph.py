aa1d154fa0c5343e7c35078a52bcbc7a9ebe54e1
Jerome-h <Jerome-h@users.noreply.github.com>
2018-06-18 14:59:07 +0100

    Updated Range finder. Comments added

--- GUI/ManualGraph.py
+++ GUI/ManualGraph.py
@@ -1,18 +1,17 @@
 import ftplib
 import csv
-import datetime
 from matplotlib import pyplot as plt
-import os
-import time
 import re
 
+# Initialise dictionaries to store data
 dictOld = dict()
 dictNew = dict()
 dictRange = dict()
+
 plt.ion()
 
-
 ########################## FUNCTIONS ######################################
+
 
 # Function to get file from the ftp server and save locally
 def getfile(ftp, filename):
@@ -22,7 +21,7 @@ def getfile(ftp, filename):
         print("Error")
 
 
-# Function to read CSV file and create a dictionary with key as time and parameter as value
+# Function to read CSV file and create a dictionary with key as the time stamp and measurement as value
 def readfile(filename, parameter, dictionary):
     with open(filename) as csvfile:
         reader = csv.DictReader(csvfile)
@@ -31,54 +30,52 @@ def readfile(filename, parameter, dictionary):
             dictionary[time] = float(row['{}'.format(parameter)])
             
 
-# function to read the header names of the file
+# Function to read and print the header names of the file, with 'Time' omitted
 def readheader(filename):
     with open(filename) as csvfile:
         reader = csv.reader(csvfile)
         print("Enter one of the following parameters:")
         headers = list(next(reader))
         for index, item in enumerate(headers):
-            if item == 'Time':
+            if item == 'Time' or item == '':
                 del(headers[index])
         print(headers)
 
 
-# Function to send data file to the FTP server
+# Function to append a data file on the FTP server
 def appendfile(ftp, filename):
     ftp.storbinary("APPE " + filename, open(filename, 'rb'))
 
 
-# Function to send file to the FTP server
+# Function to upload file to the FTP server
 def uploadfile(ftp, filename):
     ftp.storbinary("STOR " + filename, open(filename, 'rb'))
 
 
-# merging 2 dicitionaries
-def merge_two_dicts(x, y):
-    z = x.copy()   # start with x's keys and values
-    z.update(y)    # modifies z with y's keys and values & returns None
-    return z
-
-
-# function to specify the range to be graphed
+# Function creates dictionary of data within specified time range
 def getRange(startDate, endDate, dictionary, dictionaryRange):
     startFound = False
     endFound = False
+    # Key is the time stamp
     for key in sorted(dictionary):
-        if re.search(startDate, '{}'.format(key)):
+        # Determines when the key is greater than the specified start, sets bool startFound to true
+        if startDate <= key:
             startFound = True
-        if  not re.search(endDate, '{}'.format(key)) and endFound == True:
+        # If the end date is not found in the key and endFound is true, will terminate loop as all entries are found
+        if not re.search(endDate, '{}'.format(key)) and endFound == True:
             break
+        # If startFound is true the following values are within the specified range, so are added to the new dictionary
         if startFound == True:
             dictionaryRange[key] = dictionary[key]
-        if re.search(endDate, '{}'.format(key)):
-            dictionaryRange[key] = dictionary[key]
+        # Determines when the key is greater than the specified end, sets bool endFound to true
+        if endDate <= key:
             endFound = True
 
 
 # Plots the parameter dictionary as a line graph, also passes label inputs
 def graphLine(dictionary, refresh, startDate, endDate, paramType):
     xlabel = 'Date and Time'
+    # Defines graph properties dependant on parameter type specified
     if paramType == "Level":
         title = "Water Level"
         ylabel = "Level (m)"
@@ -114,8 +111,8 @@ def graphLine(dictionary, refresh, startDate, endDate, paramType):
         ylabel = "pH"
         ymax = 14
         ymin = 0
-    # haven't put in parameters for battery yet 
 
+    # Extracts dictionary into two tuples
     lists = sorted(dictionary.items())
     x, y = zip(*lists)
     xn = range(len(x))  # map your string labels to integers
@@ -127,6 +124,8 @@ def graphLine(dictionary, refresh, startDate, endDate, paramType):
     plt.xlabel('{}'.format(xlabel)) 
     plt.ylabel('{}'.format(ylabel))
     plt.title('{}'.format(title))
+
+    # Strips out labels to avoid crowding, helping user to better read the labels
     xlabels = list(x)
     for index, item in enumerate(xlabels):
         if (index % 3):
@@ -139,34 +138,36 @@ def graphLine(dictionary, refresh, startDate, endDate, paramType):
     
 ############################### MAIN CODE ####################################
 
+
 # Open ftp connection
 ftpserver = ftplib.FTP('ftp.byethost12.com', 'b12_22196264', 'equinox1234')
 
 # List the files in the current directory
 files = ftpserver.dir()
-ftpserver.cwd('/htdocs/')  # change directory to /pub/
+ftpserver.cwd('/htdocs/')  # change directory to /htdocs/
 
-# Retrieve file data2.csv
-#getfile(ftpserver, 'waterNew.csv')
+# Retrieve file from server
+# getfile(ftpserver, 'waterNew.csv')
 getfile(ftpserver, 'waterOld.csv')
 
-
-# Loop to allow user to change data. Note graph becomes unresponsive after refresh pause time has passed
+# Loop to allow user to change plot. Note graph becomes unresponsive after refresh pause time has passed
 while True:
     print('')
     # input dates
+    print("Enter the date range in the format 'year/month/day hour/minute/second: ")
     startDate = input("Start date = ")
     endDate = input("End date = ")
     # input parameter
     readheader('waterOld.csv')
     param = input("Parameter = ")
 
+    # Read file into dictionary for specified parameter
     readfile('waterOld.csv', param, dictOld)
 
-    # hold values for specific range
+    # Retrieve values for specific time range
     getRange(startDate, endDate, dictOld, dictRange)
     print(sorted(dictRange.items()))
 
-    # plot the graph
+    # Plot the graph
     graphLine(dictRange, 8, startDate, endDate, param)
 
